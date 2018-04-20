7096b84edb37316fb4914c877308b9bbd22b5893
anishk23733 <anishk23733@gmail.com>
2018-04-20 16:27:48 -0700

    sends and logs data

--- gaelscout3/teamindex/views.py
+++ gaelscout3/teamindex/views.py
@@ -4,7 +4,8 @@ from teamindex.models import *
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
-
+import json
+from raschietto import Matcher, Raschietto
 import arrow
 
 # Create your views here.
@@ -24,38 +25,41 @@ def dashboard(request, team_number):
     data = {
         'team':team,
     }
-    wins = 0
-    losses = 0
     matchesr1 = Matches.objects.filter(red1=team_number)
-    for i in matchesr1:
-        if i.winner == "R":
-            wins+=1
-        else:
-            losses+=1
     matchesr2 = Matches.objects.filter(red2=team_number)
-    for i in matchesr2:
-        if i.winner == "R":
-            wins+=1
-        else:
-            losses+=1
     matchesb1 = Matches.objects.filter(blue1=team_number)
-    for i in matchesb1:
-        if i.winner == "B":
-            wins+=1
-        else:
-            losses+=1
     matchesb2 = Matches.objects.filter(blue2=team_number)
-    for i in matchesb2:
-        if i.winner == "B":
-            wins+=1
-        else:
-            losses+=1
     try:
         t = ResearchTeams.objects.get(name=team_number)
         data['matches'] = matchesr1, matchesr2, matchesb1, matchesb2
         data['research'] = t
     except:
         pass # team is not in research division
+    scores = Raschietto.from_url("https://vexdb.io/teams/view/{}?t=rankings".format(team_number))
+    OPRPointer = Matcher('.opr')
+    OPRList = OPRPointer(scores, multiple=True)
+    try: OPRList.remove(OPRList[0])
+    except: pass
+    OPRList = list(map(float, OPRList))
+    js_opr = json.dumps(OPRList)
+
+    rankPointer = Matcher('.rank')
+    ranksList = rankPointer(scores, multiple=True)
+    try: ranksList.remove(ranksList[0])
+    except: pass
+    ranksList = list(map(int, ranksList))
+    js_ranks = json.dumps(ranksList)
+
+    maxScorePointer = Matcher('.max_score')
+    maxScoreList = maxScorePointer(scores, multiple=True)
+    try: maxScoreList.remove(maxScoreList[0])
+    except: pass
+    maxScoreList = list(map(int, maxScoreList))
+    js_mscores = json.dumps(maxScoreList)
+
+    data["js_oprs"] = js_opr
+    data["js_ranks"] = js_ranks
+    data["js_mscores"] = js_mscores
     return render(request, 'dashboard.html', data)
 
 def divisionindex(request):
