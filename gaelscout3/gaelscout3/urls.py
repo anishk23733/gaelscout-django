ee82680479864c9ea76f86c77d93446e4794853d
anishk23733 <anishk23733@gmail.com>
2018-04-20 21:45:06 -0700

    venv fix

--- gaelscout3/gaelscout3/urls.py
+++ gaelscout3/gaelscout3/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """gaelscout3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -29,34 +28,3 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<str:team_number>/dashboard/', views.dashboard, name='dashboard'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
-"""gaelscout3 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from teamindex import views
-from django.conf.urls.static import static
-from django.conf import settings
-
-urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
-    path('divisionindex/', views.divisionindex, name='divisionindex'),
-    path('matches/', views.matches, name='matches'),
-    path('teamindex/', views.teamindex, name='teamindex'),
-    path('', views.home, name='home'),
-    path('<str:team_number>/dashboard/', views.dashboard, name='dashboard'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
->>>>>>> a509b0eeadbbcc96ca7ac166aa5babfa26e24830
